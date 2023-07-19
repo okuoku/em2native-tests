e284d4d00cefea53071172639042e021e0712214
okuoku <mjt@cltn.org>
2023-07-20 00:15:33 +0900

    Update ribbon integ

---
+++ scripts/_okuokusetup.cmake
@@ -0,0 +1,25 @@
+set(mysubmodules
+    integ # => https://github.com/okuoku/em2native-integ
+    integ/coal
+    integ/cwgl
+    integ/ext/cwgl/angle-static-cmake
+    integ/nccc
+    integ/yuniframe
+    ribbon-integ
+    ribbon-integ/ribbon
+    ribbon-integ/yuni
+    ribbon-integ/yuniribbit-proto
+    )
+
+foreach(e ${mysubmodules})
+    if(e STREQUAL integ)
+        set(url "git@github.com:okuoku/em2native-integ")
+    else()
+        get_filename_component(nam "${e}" NAME)
+        set(url "git@github.com:okuoku/${nam}")
+    endif()
+    message(STATUS "${e} = ${url}")
+    execute_process(COMMAND
+        git remote set-url --push origin ${url}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/../${e})
+endforeach()
