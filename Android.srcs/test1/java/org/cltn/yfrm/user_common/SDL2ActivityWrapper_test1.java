18496b96818ba5f9cdda0228e46aecfa2402ed12
okuoku <mjt@cltn.org>
2023-07-29 19:44:53 +0900

    Android build system (template)

---
+++ Android.srcs/test1/java/org/cltn/yfrm/user_common/SDL2ActivityWrapper_test1.java
@@ -0,0 +1,12 @@
+package org.cltn.yfrm.user_common;
+
+import org.libsdl.app.SDLActivity;
+
+public class SDL2ActivityWrapper_test1 extends SDLActivity {
+    protected String[] getLibraries() {
+        return new String[] {
+            "test-imgui-so"
+        };
+    }
+
+}
